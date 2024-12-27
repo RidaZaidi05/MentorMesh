0772d86ca06da3163f5edfddea54ca649350ab3c
RidaZaidi05 <158581173+RidaZaidi05@users.noreply.github.com>
2024-12-27 14:58:40 +0500

    ok123

--- server/index.js
+++ server/index.js
@@ -69,157 +69,157 @@ app.use(cors({
   credentials: true
 }));
 
-// /* STATIC FILES */
-// app.use("/assets", express.static(path.join(__dirname, "public/assets")));
-// // app.use("/transcripts", express.static(path.join(__dirname, "public/transcripts")));
+/* STATIC FILES */
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 // app.use("/transcripts", express.static(path.join(__dirname, "public/transcripts")));
+app.use("/transcripts", express.static(path.join(__dirname, "public/transcripts")));
 
-// /* FILE STORAGE */
-// const storageAssets = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, "public/assets");
-//   },
-//   filename: function (req, file, cb) {
-//     cb(null, file.originalname);
-//   },
-// });
+/* FILE STORAGE */
+const storageAssets = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
 
-// const storageTranscripts = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, "public/transcripts");
-//   },
-//   filename: function (req, file, cb) {
-//     cb(null, file.originalname);
-//   },
-// });
+const storageTranscripts = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/transcripts");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
 
-// const uploadAssets = multer({ storage: storageAssets });
-// const uploadTranscripts = multer({ storage: storageTranscripts });
-
-
-// /* ROUTES WITH FILES */
-// app.post("/auth/register", uploadAssets.single("picture"), register);
-
-// app.post("/posts", verifyToken, uploadAssets.single("picture"), createPost);
-
-// app.post("/teacherData", uploadTranscripts.single("transcript"), createRequest);
-
-// app.post("/transData", uploadTranscripts.fields([
-//   { name: 'transcript1', maxCount: 1 },
-//   { name: 'transcript2', maxCount: 1 },
-//   { name: 'transcript3', maxCount: 1 }
-// ]), createtrans);
-
-// app.post("/acceptRequest",acceptRequestMiddleware);
+const uploadAssets = multer({ storage: storageAssets });
+const uploadTranscripts = multer({ storage: storageTranscripts });
 
 
+/* ROUTES WITH FILES */
+app.post("/auth/register", uploadAssets.single("picture"), register);
+
+app.post("/posts", verifyToken, uploadAssets.single("picture"), createPost);
+
+app.post("/teacherData", uploadTranscripts.single("transcript"), createRequest);
+
+app.post("/transData", uploadTranscripts.fields([
+  { name: 'transcript1', maxCount: 1 },
+  { name: 'transcript2', maxCount: 1 },
+  { name: 'transcript3', maxCount: 1 }
+]), createtrans);
+
+app.post("/acceptRequest",acceptRequestMiddleware);
 
 
-// /* ROUTES */
-// app.use("/auth", authRoutes);
-// app.use("/users", userRoutes);
-// app.use("/posts", postRoutes);
-// app.use("/chats", chatRoutes);
-// app.use("/chats/conversation" , conversationRoute);
-// app.use("/chats/message" , messageRoute);
 
 
-// app.use("/notification",notificationRoutes);
-// app.use("/user",userlikeroute);
-// app.use("/deleteNotifications", delelteNotification);
-// app.use("/deleteReqNotifications" , DeleteReqNotification);
-// app.use("/studentnotification", StudentNotification);
+/* ROUTES */
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
+app.use("/chats", chatRoutes);
+app.use("/chats/conversation" , conversationRoute);
+app.use("/chats/message" , messageRoute);
 
-// //---------------------------- forgot password--------------------------------------------------------------
+
+app.use("/notification",notificationRoutes);
+app.use("/user",userlikeroute);
+app.use("/deleteNotifications", delelteNotification);
+app.use("/deleteReqNotifications" , DeleteReqNotification);
+app.use("/studentnotification", StudentNotification);
+
+//---------------------------- forgot password--------------------------------------------------------------
 
 
-// app.post("/forgot-password", async (req, res) => {
-//   console.log("Inside server side code")
-//   const { email } = req.body;
-//   //console.log(email)
-//   try {
-//     const oldUser = await Student.findOne({ email });
-//     if (!oldUser) {
-//       return res.json({ status: "User Not Exists!!" });
-//     }
-//     //console.log(JWT_SECRET);
-//     const secret = JWT_SECRET + oldUser.password;
-//     const token = jwt.sign({ email: oldUser.email, id: oldUser._id }, secret, {
-//       expiresIn: "5m",
-//     });
-//     const link = `https://mentor-mesh.vercel.app/reset-password/${oldUser._id}/${token}`;
-//     var transporter = nodemailer.createTransport({
-//       service: "gmail",
-//       auth: {
-//         user: process.env.EMAIL,
-//         pass: process.env.PASS,
-//       },
-//     });
+app.post("/forgot-password", async (req, res) => {
+  console.log("Inside server side code")
+  const { email } = req.body;
+  //console.log(email)
+  try {
+    const oldUser = await Student.findOne({ email });
+    if (!oldUser) {
+      return res.json({ status: "User Not Exists!!" });
+    }
+    //console.log(JWT_SECRET);
+    const secret = JWT_SECRET + oldUser.password;
+    const token = jwt.sign({ email: oldUser.email, id: oldUser._id }, secret, {
+      expiresIn: "5m",
+    });
+    const link = `https://mentor-mesh.vercel.app/reset-password/${oldUser._id}/${token}`;
+    var transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASS,
+      },
+    });
 
-//     var mailOptions = {
-//       from: process.env.EMAIL,
-//       to: email,
-//       subject: "Password Reset Link",
-//       text: link,
-//     };
+    var mailOptions = {
+      from: process.env.EMAIL,
+      to: email,
+      subject: "Password Reset Link",
+      text: link,
+    };
 
-//     transporter.sendMail(mailOptions, function (error, info) {
-//       if (error) {
-//         console.log(error);
-//       } else {
-//         console.log("Email sent: " + info.response);
-//       }
-//     });
-//     return res.json({ status: "User Exists!!" });
-//   } catch (error) {}
-// });
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Email sent: " + info.response);
+      }
+    });
+    return res.json({ status: "User Exists!!" });
+  } catch (error) {}
+});
 
-// app.get("/reset-password/:id/:token", async (req, res) => {
-//   const { id, token } = req.params;
-//   console.log(req.params);
-//   const oldUser = await Student.findOne({ _id: id });
-//   if (!oldUser) {
-//     return res.json({ status: "User Not Exists!!" });
-//   }
-//   const secret = JWT_SECRET + oldUser.password;
-//   try {
-//     const verify = jwt.verify(token, secret);
-//     res.render("index", { email: verify.email, status: "Not Verified" });
-//   } catch (error) {
-//     console.log(error);
-//     res.send("Not Verified");
-//   }
-// });
+app.get("/reset-password/:id/:token", async (req, res) => {
+  const { id, token } = req.params;
+  console.log(req.params);
+  const oldUser = await Student.findOne({ _id: id });
+  if (!oldUser) {
+    return res.json({ status: "User Not Exists!!" });
+  }
+  const secret = JWT_SECRET + oldUser.password;
+  try {
+    const verify = jwt.verify(token, secret);
+    res.render("index", { email: verify.email, status: "Not Verified" });
+  } catch (error) {
+    console.log(error);
+    res.send("Not Verified");
+  }
+});
 
-// app.post("/reset-password/:id/:token", async (req, res) => {
-//   const { id, token } = req.params;
-//   const { password } = req.body;
+app.post("/reset-password/:id/:token", async (req, res) => {
+  const { id, token } = req.params;
+  const { password } = req.body;
 
-//   const oldUser = await Student.findOne({ _id: id });
-//   if (!oldUser) {
-//     return res.json({ status: "User Not Exists!!" });
-//   }
-//   const secret = JWT_SECRET + oldUser.password;
-//   try {
-//     const verify = jwt.verify(token, secret);
-//     const encryptedPassword = await bcrypt.hash(password, 10);
-//     await Student.updateOne(
-//       {
-//         _id: id,
-//       },
-//       {
-//         $set: {
-//           password: encryptedPassword,
-//         },
-//       }
-//     );
+  const oldUser = await Student.findOne({ _id: id });
+  if (!oldUser) {
+    return res.json({ status: "User Not Exists!!" });
+  }
+  const secret = JWT_SECRET + oldUser.password;
+  try {
+    const verify = jwt.verify(token, secret);
+    const encryptedPassword = await bcrypt.hash(password, 10);
+    await Student.updateOne(
+      {
+        _id: id,
+      },
+      {
+        $set: {
+          password: encryptedPassword,
+        },
+      }
+    );
 
-//     res.render("index", { email: verify.email, status: "verified" });
-//   } catch (error) {
-//     console.log(error);
-//     res.json({ status: "Something Went Wrong" });
-//   }
-// });
+    res.render("index", { email: verify.email, status: "verified" });
+  } catch (error) {
+    console.log(error);
+    res.json({ status: "Something Went Wrong" });
+  }
+});
 
 //---------------------------- forgot password--------------------------------------------------------------
 app.get("/", (req, res) => {
@@ -228,17 +228,12 @@ app.get("/", (req, res) => {
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
-// mongoose.connect(process.env.MONGO_URL, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// })
-// .then(() => {
-//   app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
-//   console.log("Connected to database:", mongoose.connection.db.databaseName);
-// })
-// .catch((error) => console.log(`${error} did not connect`));
-
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  console.log("Connected to database:", mongoose.connection.db.databaseName);
+})
+.catch((error) => console.log(`${error} did not connect`));
